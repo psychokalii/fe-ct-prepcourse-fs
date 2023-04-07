ad2bcf9fb7608959fcf9d55b08f931af9142a30a
RNR <111381410+psychokalii@users.noreply.github.com>
2023-04-07 03:13:15 -0300

    Update Ejercicio 04.js

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -13,15 +13,47 @@ function elevarAlCubo(num) {
    // Tu código:
 }
 
+
+     
+    function elevarAlCuadrado(num) {
+    var potencia = num ** 2;
+    console.log(potencia);
+    }
+
+    elevarAlCuadrado(3)
+
+
+
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
 }
 
+
+    function elevar(num, exponent) {
+    var exponente = num ** exponent;
+    console.log(exponente);
+    }
+
+    elevar(2, 3) 
+
+
+
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
 }
+
+
+
+    function redondearNumero(num) {
+    var redondear = Math.round(num);
+    console.log(redondear);
+    }
+
+    redondearNumero(2.5)
+     
+
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
@@ -32,6 +64,16 @@ function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
 }
+
+
+    function numeroRandom() {
+    Math.random;
+    }
+
+    numeroRandom()    
+
+
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {

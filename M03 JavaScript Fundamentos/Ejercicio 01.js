eb1dbbd1f79a96c07ff68803581bf3a7d50e441d
RNR <111381410+psychokalii@users.noreply.github.com>
2023-04-07 00:38:15 -0300

    Update Ejercicio 01.js

--- M03 JavaScript Fundamentos/Ejercicio 01.js
+++ M03 JavaScript Fundamentos/Ejercicio 01.js
@@ -7,20 +7,48 @@
 // Crea una variable de tipo string.
 const nuevoString = null;
 
+
+  var nuevoString = "Hola";
+
+
 // Crea una variable de tipo number.
 const nuevoNumero = null;
+
+
+ var nuevoNumero = 100;
+
 
 // Crea una variable de tipo boolean.
 const nuevoBoolean = null;
 
+
+  var nuevoBoolean = true;
+
+
 // Resuelve el siguiente problema matemático.
 const nuevaResta = 10 - null === 3;
+
+
+  var nuevaResta = 10 - (3 === 3);
+  console.log(nuevaResta);
+
 
 // Resuelve el siguiente problema matemático.
 const nuevaMultiplicacion = 10 * null === 40;
 
+
+  var nuevaMultiplicacion = 10 * (40 === 40);
+  console.log(nuevaMuletiplicacion);
+  
+
+
 // Resuelve el siguiente problema matemático.
 const nuevoModulo = 21 % 5 === null;
+
+
+  var nuevoModulo = 21 % (5 === 5);
+  console.log(nuevoModulo);
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {

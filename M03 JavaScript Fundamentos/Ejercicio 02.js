1bc9bb1a0812558f6f35518c5db594b1dea49d6d
RNR <111381410+psychokalii@users.noreply.github.com>
2023-04-07 01:07:02 -0300

    Update Ejercicio 02.js

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -6,6 +6,16 @@ function devolverString(string) {
    // Tu código:
 }
 
+   
+    function devolverString(string) {
+        console.log(string);
+    }
+ 
+
+     devolverString('Rocio')
+     
+
+
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
@@ -13,25 +23,69 @@ function suma(x, y) {
    // Tu código:
 }
 
-function resta(x, y) {
-   // Retorna el resultado de la resta.
-   // Tu código:
-}
 
+     function suma(x, y) {
+         console.log( x + y);
+     }
+
+     suma(100, 200)
+
+
+
+
+   // Retorna el resultado de la resta.
+   // Tu código:}
+
+
+      
+     function resta(x, y) {
+         console.log(x - y);
+     }
+
+     resta(200, 100)
+      
+   
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
 }
+
+
+     function divide(x, y) {
+        console.log(x / y);
+     }
+
+     divide(9, 3)
+
+
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
 }
 
+
+     function multiplica(x, y) {
+        console.log(x * y);
+     }
+  
+     multiplica(5,5)
+
+
+
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
 }
+
+
+    function obtenerResto(x, y) {
+       console.log(x / y);
+    }
+
+    obtenerResto(100,5)
+
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
